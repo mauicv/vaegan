fb29d40a62790443e6f28a84570cf73e85f00c7a
Alex Athorne <a.thornysort@gmail.com>
2022-11-28 20:23:10 +0000

    Add imports to init files

--- duct/utils/dataset.py
+++ duct/utils/dataset.py
@@ -10,7 +10,7 @@ from torchvision import transforms
 class CelebADataset(Dataset):
     def __init__(
             self,
-            root='data/celeba/',
+            root='./datasets/celeba/',
             annotations_file='identity_CelebA.txt',
             img_dir='img_align_celeba',
             transform=None,
@@ -34,20 +34,18 @@ class CelebADataset(Dataset):
         return image
 
 
-def get_dataset(config):
-
-    if not Path('./dataset/celeba/img_align_celeba.zip').is_file():
-        raise ValueError('Dataset not downloaded, run `python main.py dl`'
-                         'first!')
+def get_dataset(path='./datasets/celeba', img_shape=(3, 128, 128)):
+    if not Path(path / 'img_align_celeba.zip').is_file():
+        raise ValueError('Dataset not downloaded')
 
     dataset = CelebADataset(
-        root='./dataset/celeba',
+        root=path,
         annotations_file='identity_CelebA.txt',
         img_dir='img_align_celeba',
         transform=transforms.Compose([
             transforms.CenterCrop([128, 128]),
             transforms.ConvertImageDtype(torch.float32),
-            transforms.Resize(config['VAE_PARAMS']['img_shape'])
+            transforms.Resize(img_shape)
         ])
     )
 
--- duct/utils/download.py
+++ duct/utils/download.py
@@ -4,16 +4,16 @@ import gdown
 
 
 def download_dataset():
-    path = Path('./dataset')
+    path = Path('./datasets')
     path.mkdir(exist_ok=True)
-    path = Path('./dataset/celeba')
+    path = Path('./datasets/celeba')
     path.mkdir(exist_ok=True)
 
     base_url = 'https://storage.googleapis.com/celeba-data-aa'
     dataset_url = f'{base_url}/img_align_celeba.zip'
     annotate_url = f'{base_url}/identity_CelebA.txt'
 
-    target_loc_imgs = Path("./dataset/celeba/img_align_celeba.zip")
+    target_loc_imgs = Path("./datasets/celeba/img_align_celeba.zip")
     if target_loc_imgs.is_file():
         print('Dataset already downloaded')
     else:
@@ -23,7 +23,7 @@ def download_dataset():
             quiet=False
         )
 
-    target_loc_idents = Path("./dataset/celeba/identity_CelebA.txt")
+    target_loc_idents = Path("./datasets/celeba/identity_CelebA.txt")
     if target_loc_idents.is_file():
         print('Identities already downloaded')
     else:
@@ -33,9 +33,9 @@ def download_dataset():
             quiet=False
         )
 
-    target_loc_unzipped_imgs = Path("./dataset/celeba/img_align_celeba")
+    target_loc_unzipped_imgs = Path("./datasets/celeba/img_align_celeba")
     if not target_loc_unzipped_imgs.is_dir():
         with zipfile.ZipFile(target_loc_imgs, 'r') as ziphandler:
-            ziphandler.extractall('./dataset/celeba')
+            ziphandler.extractall('./datasets/celeba')
 
     return str(target_loc_imgs)
