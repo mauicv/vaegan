d97ba2112fc5902189b9c971af685d1d1aeff81e
Alex Athorne <a.thornysort@gmail.com>
2022-11-28 23:23:50 +0000

    Split download and unzip into seperate methods

--- duct/utils/download.py
+++ duct/utils/download.py
@@ -3,17 +3,17 @@ from pathlib import Path
 import gdown
 
 
-def download_dataset():
-    path = Path('./datasets')
+def download_dataset(dl_loc='datasets/'):
+    path = Path(dl_loc)
     path.mkdir(exist_ok=True)
-    path = Path('./datasets/celeba')
+    path = path / 'celeba'
     path.mkdir(exist_ok=True)
 
     base_url = 'https://storage.googleapis.com/celeba-data-aa'
     dataset_url = f'{base_url}/img_align_celeba.zip'
     annotate_url = f'{base_url}/identity_CelebA.txt'
 
-    target_loc_imgs = Path("./datasets/celeba/img_align_celeba.zip")
+    target_loc_imgs = path / 'img_align_celeba.zip'
     if target_loc_imgs.is_file():
         print('Dataset already downloaded')
     else:
@@ -23,7 +23,7 @@ def download_dataset():
             quiet=False
         )
 
-    target_loc_idents = Path("./datasets/celeba/identity_CelebA.txt")
+    target_loc_idents = path / 'identity_CelebA.txt'
     if target_loc_idents.is_file():
         print('Identities already downloaded')
     else:
@@ -33,9 +33,12 @@ def download_dataset():
             quiet=False
         )
 
-    target_loc_unzipped_imgs = Path("./datasets/celeba/img_align_celeba")
+def unzip_dataset(path='datasets'):
+    path = Path(path) / 'celeba'
+    target_loc_imgs = path / 'img_align_celeba.zip'
+    target_loc_unzipped_imgs = path / 'img_align_celeba'
     if not target_loc_unzipped_imgs.is_dir():
         with zipfile.ZipFile(target_loc_imgs, 'r') as ziphandler:
-            ziphandler.extractall('./datasets/celeba')
+            ziphandler.extractall(target_loc_unzipped_imgs)
 
     return str(target_loc_imgs)
