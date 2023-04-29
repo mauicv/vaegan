f2d2b84ffa19e389d351ab2bf91be06bb6f046ad
Alex Athorne <a.thornysort@gmail.com>
2023-04-29 10:33:15 +0100

    fix v_weight, v_basis and cells issue in concept_block

--- duct/model/transformer/encoding_block.py
+++ duct/model/transformer/encoding_block.py
@@ -1,10 +1,7 @@
 import torch
 import torch.nn as nn
-
-
-import torch
-from torch import nn
 from torch.nn import Parameter
+
 
 class ConceptEncodingBlock(nn.Module):
 		def __init__(self, m, emb_dim, n_heads, use_bias=True):
@@ -17,9 +14,9 @@ class ConceptEncodingBlock(nn.Module):
 			self.cells = Parameter(torch.zeros(self.m, self.n_heads, self.head_size))
 			self.cells.data.uniform_(-1/self.m, 1/self.m)
 			self.q = torch.nn.Linear(emb_dim, emb_dim)
-			self.v = Parameter(torch.randn(self.m, emb_dim, emb_dim))
+			self.v_weights = Parameter(torch.randn(self.m, emb_dim, emb_dim))
 			if use_bias:
-				self.vb = Parameter(torch.randn(self.m, emb_dim))
+				self.v_basis = Parameter(torch.randn(self.m, emb_dim))
 			self.norm = nn.LayerNorm(emb_dim)
 			self.attn_drop = nn.Dropout(0.1)
 
@@ -29,10 +26,10 @@ class ConceptEncodingBlock(nn.Module):
 			q = self.q(h_) \
 				.reshape(-1, l, self.n_heads, self.head_size) \
 				.transpose(1,2) # b, nh, l, hs
-			v_ = torch.einsum('mwv,blv->bmlw', self.v, h_) \
+			v_ = torch.einsum('mwv,blv->bmlw', self.v_weights, h_) \
 					.reshape(-1, self.m, l, self.n_heads, self.head_size) # b, m, l, nh, hs
 			if hasattr(self, 'vb'):
-				v_ = v_ + self.vb.reshape(1, self.m, 1, self.n_heads, self.head_size) # b, m, l, nh, hs
+				v_ = v_ + self.v_basis.reshape(1, self.m, 1, self.n_heads, self.head_size) # b, m, l, nh, hs
 
 			# compute attention
 			w_ = torch.einsum('bhlv,mhv->bhml', q, self.cells) # b, nh, m, l
